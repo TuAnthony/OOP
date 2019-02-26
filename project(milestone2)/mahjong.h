e1ea0f0c548e066d9ec0378c92005f03f5ca2732
TuAnthony <46986403+TuAnthony@users.noreply.github.com>
2019-02-26 16:07:24 -0500

    rough code

---
+++ project(milestone2)/mahjong.h
@@ -0,0 +1,26 @@
+#ifndef MAHJONG_H_INCLUDED
+#define MAHJONG_H_INCLUDED
+
+class Map
+{
+    public:
+        int n;
+
+    private:
+
+};
+
+class moving
+{
+
+    public:
+        int mouseButton;
+        int moves;
+
+    private:
+
+
+};
+
+
+#endif // MAHJONG_H_INCLUDED
