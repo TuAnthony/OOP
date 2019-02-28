80bbc6d1bd47b37d5db0159e1ae5a63113342f79
TuAnthony <46986403+TuAnthony@users.noreply.github.com>
2019-02-27 23:58:06 -0500

    updated

--- Project Milestone2/snake.hpp
+++ Project Milestone2/snake.hpp
@@ -1,6 +1,25 @@
 #ifndef SNAKE_HPP_INCLUDED
 #define SNAKE_HPP_INCLUDED
 
+
+class window{
+
+    public:
+    int draw;
+    int restart;
+    int timer;
+    Window get_window() const {return w;}
+    Event get_e() const {return e;}
+
+
+    private:
+
+    Window window;
+    Event e;
+
+};
+
+
 struct Snake
 {
   int x, y;
