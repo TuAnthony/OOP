2bce4c969119ae8c5da9a21995de882e9bc5cabf
TuAnthony <46986403+TuAnthony@users.noreply.github.com>
2019-04-07 12:38:19 -0400

    made changes

--- Project Milestone 3/snake.cpp
+++ Project Milestone 3/snake.cpp
@@ -42,69 +42,119 @@ void windows::Tick()
       num = i;
 }
 
-//function generates a random number using srand and creates the game window
-void windows::createWindow()
+//Precondition:     This function takes in no parameters.
+//Postcondition:    This function returns void and creates the game window.
+void Windows::CreateWindow()
 {
+    sf::Music music;
 
-  srand(time(0));
+    //Check to see if music file was loaded properly
+    if(!music.openFromFile("/Users/Turner/Desktop/Snake/images/Seed.wav"))
+        std::cout << "Error Loading Music" << std::endl;
 
-  RenderWindow window(VideoMode(w, h), "Snake Game!");
+    //Generate a random number
+    srand(static_cast<unsigned int>(time(NULL)));
 
-  Texture t1, t2;
-  t1.loadFromFile("images/white.png");
-  t2.loadFromFile("images/red.png");
+    //Create the game Window
+    sf::RenderWindow window(sf::VideoMode(width, height), "Snake");
+    sf::Texture texture1, texture2, texture3, texture4, texture5, texture6;
 
-  Sprite sprite1(t1);
-  Sprite sprite2(t2);
+    //Load the images used for the game
+    //Four colours were added
+    texture1.loadFromFile("/Users/Anthony/Desktop/Snake/images/red.png");
+    texture2.loadFromFile("/Users/Anthony/Desktop/Snake/images/white.png");
+    texture3.loadFromFile("/Users/Anthony/Desktop/Snake/images/maroon.png");
+    texture4.loadFromFile("/Users/Anthony/Desktop/Snake/images/purple.png");
+    texture5.loadFromFile("/Users/Anthony/Desktop/Snake/images/yellow.png");
+    texture6.loadFromFile("/Users/Anthony/Desktop/Snake/images/blue.png");
 
-  Clock clock;
-  float timer = 0, delay = 0.1;
+    sf::Sprite sprite1(texture1), sprite2(texture2), sprite3(texture3), sprite4(texture4), sprite5(texture5), sprite6(texture6);
+    sf::Clock clock;
 
-  f.x = 10;
-  f.y = 10;
+    f.x = 10;
+    f.y = 10;
 
-  while (window.isOpen()) {
+    int var = 0;
+    bool decision = true;
+
+
     float time = clock.getElapsedTime().asSeconds();
+
     clock.restart();
     timer += time;
 
-    Event e;
-    while (window.pollEvent(e)) {
-      if (e.type == Event::Closed)
-        window.close();
-    }
+    sf::Event event;
 
-    if (Keyboard::isKeyPressed(Keyboard::Left))
-      dir = 1;
-    if (Keyboard::isKeyPressed(Keyboard::Right))
-      dir = 2;
-    if (Keyboard::isKeyPressed(Keyboard::Up))
-      dir = 3;
-    if (Keyboard::isKeyPressed(Keyboard::Down))
-      dir = 0;
+    while(window.pollEvent(event))
 
-    if (timer > delay) {
-      timer = 0;
-      Tick();
-    }
+        if(event.type == sf::Event::Closed)
+            window.close();
 
-    ////// draw  ///////
-    window.clear();
+        //Create the keyboard controls
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            dir = 1;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            dir = 2;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            dir = 3;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            dir = 0;
+        if(timer > delay)
+        {
+            timer = 0;
 
-    for (int i = 0; i < N; i++)
-      for (int j = 0; j < M; j++) {
-        sprite1.setPosition(i * size, j * size);
-        window.draw(sprite1);
-      }
+            //Call Tick() class function
+            Tick();
+        }
 
-    for (int i = 0; i < num; i++) {
-      sprite2.setPosition(s[i].x * size, s[i].y * size);
-      window.draw(sprite2);
-    }
+        window.clear();
 
-    sprite2.setPosition(f.x * size, f.y * size);
-    window.draw(sprite2);
+        for(int counter = 0; counter < N; ++counter)
+            for(int innerCounter = 0; innerCounter < M; ++innerCounter)
+            {
+                sprite1.setPosition(counter * size, innerCounter * size);
+                window.draw(sprite1);
+            }
+        for(int counter = 0; counter < num; ++counter)
+        {
+            sprite2.setPosition(s[counter].x * size, s[counter].y * size);
+            window.draw(sprite2);
+        }
 
-    window.display();
-  }
+        var = num % 5;
+
+        //Depending on the length of the snake, the food changes colour
+        switch(var)
+        {
+            case 1:
+                sprite3.setPosition(f.x * size, f.y * size);
+                window.draw(sprite3);
+                decision = true;
+                break;
+            case 2:
+                sprite4.setPosition(f.x * size, f.y * size);
+                window.draw(sprite4);
+                decision = true;
+                break;
+            case 3:
+                sprite5.setPosition(f.x * size, f.y * size);
+                window.draw(sprite5);
+                if(decision == true)
+                    delay = delay - 0.01;
+                decision = false;
+                break;
+            case 4:
+                sprite6.setPosition(f.x * size, f.y * size);
+                window.draw(sprite6);
+                decision = true;
+                break;
+            case 0:
+            case 5:
+                sprite2.setPosition(f.x * size, f.y * size);
+                window.draw(sprite2);
+                decision = true;
+                break;
+        }
+
+        window.display();
 }
